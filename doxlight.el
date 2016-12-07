2bf77652d7d818805c2f1fd81b9450462aea74dc
Steffen Fiedler <mail@steffenfiedler.com>
2016-12-07 08:13:54 +0100

    updates highlighting

--- doxlight.el
+++ doxlight.el
@@ -5,40 +5,41 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; doxlight function keywords
-(setq doxlight-keywords-token '("code" "endcode" "endhtmlonly" "example" "htmlonly" "image" "include" "link" "mainpage" "name" "note" "page" "paragraph" "ref" "section" "snippet" "subpage" "subsection" "todo"))
-
-;; Prepend doxlight tokens with @ to match
-;; complete declaration, not only the name.
-(setq doxlight-keywords-token-prepend (mapcar (lambda (k)
-                                         (concat "@" k))
-                                       doxlight-keywords-token))
-
-;; generate regex string for each category of keywords
-(setq doxlight-keywords-regexp (regexp-opt doxlight-keywords-token-prepend t))
-
-;; generate regex string to match label behind doxygen tokens (e.g. ref)
-;; >>> FIXME, @ref not highlighted
-;; (?<=@ref\s)\w+
-(setq doxlight-label-regexp "@ref[[:space:]]+\\([A-Z-a-z0-9_-]+\\)")
-
 ;; create the list for font-lock.
-;; each category of keyword is given a particular face
 (setq doxlight-font-lock-keywords
-      `((,doxlight-keywords-regexp . font-lock-function-name-face)
-        (,doxlight-label-regexp . font-lock-constant-face)))
+      `(;; Match single commands without parameters or meta `@page`
+        (,"@\\(endhtmlonly\\|htmlonly\\|ingroup\\|internal\\|mainpage\\|name\\|note\\|page\\|paragraph\\|todo\\)[[:space:]]" . font-lock-keyword-face)
+        ;; Match document-referencing commands followed by file name `@example foo.c`
+        (,"\\(@\\(example\\|snippet\\)\\)[[:space:]]+\\([A-Za-z0-9_-\.]+\\)*"
+         (1 font-lock-keyword-face)
+         (3 font-lock-constant-face))
+        ;; Match commands followed by a label `@section label_token`
+        (,(concat "\\(@\\(addtogroup\\|example\\|image\\|section\\|subsection\\)\\)"
+                  ;; One or more whitespaces
+                  "[[:space:]]+"
+                  ;; Match label token
+                  "\\([A-Z-a-z0-9_-]+\\)")
+         (1 font-lock-keyword-face)
+         (3 font-lock-function-name-face))
+        ;; Match commands followed by label and string in quotation marks `@ref label_token "caption string"`
+        (,(concat "\\(@\\(\\defgroup\\|ref\\|subpage\\)\\)"
+                  ;; One or more whitespaces
+                  "[[:space:]]+"
+                  ;; Match label token
+                  "\\([A-Z-a-z0-9_-]+\\)"
+                  ;; One or more whitespaces
+                  "[[:space:]]+"
+                  ;; Optional caption `"bold terms"`
+                  "\\(\"\\(\\(?:[^\"\\]+\\|\\\\\\(?:.\\|\\)\\)*\\)\"\\)*")
+         (1 font-lock-keyword-face)
+         (3 font-lock-function-name-face)
+         (4 font-lock-string-face nil t))))
 
 (define-derived-mode doxlight-mode text-mode "doxlight"
-  "Major mode for editing doxlight files."
+  "Major mode for editing doxygen files."
   (setq font-lock-defaults '(doxlight-font-lock-keywords)))
 
 ;; Autoload
 (add-to-list 'auto-mode-alist '("\\.dox\\'" . doxlight-mode))
-
-;; Clear memory
-(setq doxlight-keywords-token nil)
-(setq doxlight-keywords-token-prepend nil)
-(setq doxlight-keywords-regexp nil)
-(setq doxlight-label-regexp nil)
 
 (provide 'doxlight-mode)
